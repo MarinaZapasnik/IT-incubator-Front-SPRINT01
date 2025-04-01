ae53d2af77a6a82354009b9ca8a29d5b9ebc5a0a
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2025-04-01 23:08:22 +0300

    tests and  objects types description

---
+++ src/NativeJS-TS-vith-Dimych/02-hello-tests/02_02.ts
@@ -0,0 +1,29 @@
+
+type GovernmentBuildingsType = {
+    type: string
+    budget: number
+    staffCount: number
+    address: AddressType
+}
+
+type StreetType = {
+        title: string
+}
+
+type AddressType = {
+    number?: number
+    street: StreetType
+}
+
+type HousesType = {
+    buildedAt: number
+    repaired: boolean
+    address: AddressType
+}
+
+export type CityType = {
+    title: string
+    houses: Array<HousesType>
+    governmentBuildings: Array<GovernmentBuildingsType>
+    citizensNumber: number
+}
